2953fb76505412e04e563135ec827af6b120903c
Lê Minh Trí <leminhtri11a1@gmail.com>
2024-07-19 14:42:55 +0700

    feat(build): Add PostCSS plugins for autoprefixing and import
    Adds the following PostCSS plugins to the project:
    
    - postcss-import: Imports CSS files.
    - autoprefixer: Adds vendor prefixes to CSS properties.
    
    This commit also configures the PostCSS plugins to be used in both development and production environments.
    
    Authored-by: Lê Minh Trí <leminhtri11a1@gmail.com>

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
     plugins: {
+        "postcss-import": {},
         tailwindcss: {},
+        autoprefixer: {},
+        "tailwindcss/nesting": {},
+        ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
     },
 };
 
